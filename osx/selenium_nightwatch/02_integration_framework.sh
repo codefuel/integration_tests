db9172a0d7446b0b1d3a4f6f38c80b300e905ef6
codefuel <codefuel@users.noreply.github.com>
2017-11-29 07:56:21 -0800

    Update selenium installation to use npm rather than homebrew.

--- osx/selenium_nightwatch/02_integration_framework.sh
+++ osx/selenium_nightwatch/02_integration_framework.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# TODO: figure out how to automatically start selenium when installing homebrew dependencies
 # Homebrew
-echo "Updating Homebrew formulas ..."
-brew update
-brew tap Homebrew/bundle
+# echo "Updating Homebrew formulas ..."
+# brew update
+# brew tap Homebrew/bundle
+#
+# brew bundle --file=./Brewfile.integration
 
-brew bundle --file=./Brewfile.integration
+npm install selenium-standalone@latest -g
+selenium-standalone install
+
+echo "*** Add paths for selenium, chrome driver, and gecko driver to nightwatch.json"
 
 npm install --global nightwatch
